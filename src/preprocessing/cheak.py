37a301a6f93b233fee4cac56b46b57609ccc8a5c
workwithsoham62 <workwithsoham629@gmail.com>
2026-07-13 13:36:31 +0530

    feat: Update .gitignore, enhance README structure, and add data exploration scripts

---
+++ src/preprocessing/cheak.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\soham\Downloads\ISI Project\data\processed\friday_clean.csv")
+
+# Remove only the label column
+X = df.drop(columns=["Label"])
+
+print("="*50)
+print("Duplicate feature vectors")
+print("="*50)
+
+print(X.duplicated().sum())
